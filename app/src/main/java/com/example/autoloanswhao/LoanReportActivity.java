77da9aa1d76da4e0d0e07b37ac41a668ba90bac2
seamu.th2363 <seamu.th2363@vusd.us>
2022-09-14 15:27:31 -0700

    Layouts Done TODO push Loan object between layouts

--- app/src/main/java/com/example/autoloanswhao/LoanReportActivity.java
+++ app/src/main/java/com/example/autoloanswhao/LoanReportActivity.java
@@ -4,14 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class LoanReportActivity extends AppCompatActivity {
+
+    private TextView foundCarPrice, foundDownPayment, foundTradeInValue, foundSalesTax;
+    private TextView foundTotalLoan, foundTotalInterestPaid, foundTotalAmount, foundMonthlyPayment;
+    // ^ only on a different line so no sideways scrolling
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_loan_report);
+
+        foundCarPrice = findViewById(R.id.foundCarPrice);
+        foundDownPayment = findViewById(R.id.foundDownPayment);
+        foundTradeInValue = findViewById(R.id.foundTradeInValue);
+        foundSalesTax = findViewById(R.id.foundSalesTax);
+        foundTotalLoan = findViewById(R.id.foundTotalLoan);
+        foundTotalInterestPaid = findViewById(R.id.foundTotalInterestPaid);
+        foundTotalAmount = findViewById(R.id.foundTotalLoan);
+        foundMonthlyPayment = findViewById(R.id.foundMonthlyPayment);
+
     }
+
 
     public void goDataEntry(View view){
         finish();
--- app/src/main/java/com/example/autoloanswhao/MainActivity.java
+++ app/src/main/java/com/example/autoloanswhao/MainActivity.java
@@ -5,22 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
 //https://www.cars.com/car-loan-calculator/
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText tilCarPrice;
+    private TextInputEditText tilDownPayment;
+    private TextInputEditText tilTradeInValue;
+    private TextInputEditText tilAnnualPercentage;
+    private TextInputEditText tilSalesTaxRate;
+
+    private RadioGroup lengthOfLoanGroup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lengthOfLoanGroup = findViewById(R.id.lengthOfLoanGroup);
+
+        tilCarPrice =  findViewById(R.id.tilCarPrice);
+        tilDownPayment =  findViewById(R.id.tilDownPayment);
+        tilTradeInValue =  findViewById(R.id.tilTradeInValue);
+        tilAnnualPercentage =  findViewById(R.id.tilAnnualPercentageRate);
+        tilSalesTaxRate =  findViewById(R.id.tilSalesTaxRate);
     }
+
+
 
     public void goToLoanReport(View view){
 
-        AutoLoan loan = new AutoLoan();
+
+        int termId = lengthOfLoanGroup.getCheckedRadioButtonId();
+        RadioButton termButton = findViewById(termId);
+        try{
+            double price = Double.valueOf(tilCarPrice.getText().toString());
+            double downPayment = Double.valueOf(tilDownPayment.getText().toString());
+            double tradeInValue = Double.valueOf(tilTradeInValue.getText().toString());
+            int length = Integer.parseInt(termButton.getText().toString());
+            double annualPercentage = Double.valueOf(tilAnnualPercentage.getText().toString());
+            double salesTaxRate = Double.valueOf(tilSalesTaxRate.getText().toString());
+
+            AutoLoan loan = new AutoLoan(price, downPayment, tradeInValue, length, annualPercentage, salesTaxRate);
+
+            Intent intent = new Intent(this, LoanReportActivity.class);
+            startActivity(intent);
+        }
+        catch (Exception e){
+            Toast.makeText(this, "Invalid Entry. RE-ATTEMPT PLEASE", Toast.LENGTH_SHORT).show();
+        }
 
 
 
-        Intent intent = new Intent(this, LoanReportActivity.class);
-        startActivity(intent);
     }
 }
